88af270849a3e3bfc465739cc7257252d2252839
go5010 <gota.4305@gmail.com>
2023-12-25 23:41:41 +0900

    part47

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 export const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px",
+    margin: 100,
+  };
+
   return (
     <>
-      <h1>こんにちは！</h1>
+      <h1 style={contentStyle}>こんにちは！</h1>
       <p>お元気ですか？</p>
+      {console.log("hoge")}
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
