84d2772f27970d4e1e61721d8e222632938e14aa
go5010 <gota.4305@gmail.com>
2023-12-25 19:55:02 +0900

    part45

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<StrictMode><h1>こんにちは！</h1></StrictMode>);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
