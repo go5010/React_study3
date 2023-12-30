98d3b015924d1a1bdd599cb57ea9a1c149f9759a
go5010 <gota.4305@gmail.com>
2023-12-30 12:00:29 +0900

    part50完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+  console.log("--App--");
   const [num, setNum] = useState(0);
   const [isShowFace, seIsShowFace] = useState(true);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1);
-    setNum((prev) => prev + 1);
   };
+  const onClickToggle = () => {
+    seIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || seIsShowFace(true);
+      } else {
+        isShowFace && seIsShowFace(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -16,8 +29,8 @@ export const App = () => {
       <ColorfulMessage color="green">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
-      <button>on/off</button>
-      {isShowFace && <p>aiu</p>}
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>(^o^)</p>}
     </>
   );
 };
